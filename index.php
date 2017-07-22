c0723e73d228abd83e9540765cd5a911e72a19e4
Milan <meleran82@gmail.com>
2017-07-23 00:38:54 +0545

    First commit by Milan

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+/**
+*
+*@link http://adhikarimilan.com.np/
+*
+*@package WordPress
+*@subpackage wptheme
+*@since 1.0.0
+*@version 1.0.0
+*
+*/
+ ?>
+
+ <!DOCTYPE html>
+ <html <?php language_attributes();?>>
+ <head>
+ 	<title><?php get_bloginfo('title'); ?></title>
+ 	<meta charset="<?php bloginfo('charset'); ?>">
+ 	<meta name="description" content="<?php bloginfo('description'); ?>">
+ 	<?php wp_head(); ?>
+ </head>
+ <body>
+ 	<?php echo "The author of this file is Milan Adhikari"; ?>
+ </body>
+ </html>
